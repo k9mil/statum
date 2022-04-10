d296a0a4ddcaa16c439da336a7d696a8ada998fb
Kamil Zak <kamilzak00@gmail.com>
2022-04-10 21:03:17 +0100

    refactor: forgot to git add .

--- statum/main/utils/scheduled.py
+++
@@ -1,8 +0,0 @@
-from statum import scheduler, create_app
-from statum.main.utils.utils import random_stream
-
-@scheduler.task('interval', id='periodic_index_clearance', seconds=300, misfire_grace_time=360)
-def periodic_index_clearance():
-    app = create_app()
-    with app.test_request_context():
-        random_stream()
