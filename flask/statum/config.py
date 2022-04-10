df204632ceeea397e0412fa40a7923dbf88514c7
Kamil Zak <kamilzak00@gmail.com>
2022-04-10 04:06:39 +0100

    feat: complete re-factor of code, added nginx + uwsgi + docker support for prod

---
+++ flask/statum/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    AUTH_KEY = os.environ.get('AUTH_KEY')
+    CLIENT_ID = os.environ.get('CLIENT_ID')
+    SCHEDULER_API_ENABLED = True
+    LOGIN_URL = f"https://id.twitch.tv/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri=http://localhost:80/dashboard&response_type=code&scope=openid+user:read:email&claims={'id_token'}"
