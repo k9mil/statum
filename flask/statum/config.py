951b98901b6fad0fdd1dc8db557a5939f6cf53cb
Kamil Zak <kamilzak00@gmail.com>
2022-04-10 04:10:34 +0100

    fix: fix config for prod

--- flask/statum/config.py
+++ flask/statum/config.py
@@ -4,5 +4,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     AUTH_KEY = os.environ.get('AUTH_KEY')
     CLIENT_ID = os.environ.get('CLIENT_ID')
+    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+    LOGIN_URL = f"https://id.twitch.tv/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri=https://46.101.62.143/dashboard&response_type=code&scope=openid+user:read:email&claims={'id_token'}"
     SCHEDULER_API_ENABLED = True
-    LOGIN_URL = f"https://id.twitch.tv/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri=http://localhost:80/dashboard&response_type=code&scope=openid+user:read:email&claims={'id_token'}"
