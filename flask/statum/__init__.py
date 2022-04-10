54991d03f7f6f61ad9a455b1cc741000db2cd8d6
Kamil Zak <kamilzak00@gmail.com>
2022-04-10 11:24:29 +0100

    fix: fix mongodb init + typos

--- flask/statum/__init__.py
+++ flask/statum/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_apscheduler import APScheduler
 from statum.config import Config
-import pymongo
+import pymongo, certifi
 
-client = pymongo.MongoClient("localhost", 27017)
+ca = certifi.where()
+client = pymongo.MongoClient(f"mongodb+srv://statum:{Config.MONGO_PASSWORD}@statum.c5zu0.mongodb.net/statum_db?retryWrites=true&w=majority", tlsCAFile=ca)
 database = client.statum_db
 scheduler = APScheduler()
 scheduler.start()
@@ -17,5 +18,5 @@ def create_app(config_class=Config):
 
     app.register_blueprint(main)
     app.register_blueprint(errors)
-	
+
     return app
--- flask/statum/config.py
+++ flask/statum/config.py
@@ -5,5 +5,5 @@ class Config:
     AUTH_KEY = os.environ.get('AUTH_KEY')
     CLIENT_ID = os.environ.get('CLIENT_ID')
     MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
-    LOGIN_URL = f"https://id.twitch.tv/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri=https://http://statum-demo.tk//dashboard&response_type=code&scope=openid+user:read:email&claims={'id_token'}"
+    LOGIN_URL = f"https://id.twitch.tv/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri=https://statum-demo.tk/dashboard&response_type=code&scope=openid+user:read:email&claims={'id_token'}"
     SCHEDULER_API_ENABLED = True
