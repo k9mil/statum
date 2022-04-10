664efd6cac2810b9b173e0a16e4a91c155912219
Kamil Zak <kamilzak00@gmail.com>
2022-04-10 04:28:32 +0100

    fix: accidental folder

--- statum/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    AUTH_KEY = os.environ.get('AUTH_KEY')
-    CLIENT_ID = os.environ.get('CLIENT_ID')
-    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
-    LOGIN_URL = f"https://id.twitch.tv/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri=https://statoom.herokuapp.com/dashboard&response_type=code&scope=openid+user:read:email&claims={'id_token'}"
-    SCHEDULER_API_ENABLED = True
